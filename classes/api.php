e95fb73ada09a42918dfd12a301ace22d451d797
CarlosTMJ <61996695+carlostmjcarlostmj2019@users.noreply.github.com>
2024-05-10 23:46:10 -0300

    Create api.php

--- classes/api.php
+++ classes/api.php
@@ -1,49 +1,31 @@
 <?php
+// classes/api.php
 
-class Acesso {
-    private $conexao; // Conexão com o banco de dados
+class API {
+    private $curl;
+    private $url_api = 'https://brasilapi.com.br/api/';
 
-    public function __construct($conexao) {
-        $this->conexao = $conexao;
+    public function __construct() {
+        $this->curl = new Curl();
     }
 
-    public function login($usuario, $senha) {
-        // Preparando a consulta SQL com uma instrução preparada (stmt)
-        $sql = "SELECT id, nome, email, usuario, senha FROM usuarios WHERE usuario = ? AND senha = ?";
-        $stmt = $this->conexao->prepare($sql);
-
-        // Verificando se a preparação da consulta foi bem-sucedida
-        if ($stmt === false) {
-            return null; // Retorna null se houve um erro na preparação da consulta
-        }
-
-        // Criptografando a senha
-        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-
-        // Ligando os parâmetros da consulta
-        $stmt->bind_param("ss", $usuario, $senhaHash);
-
-        // Executando a consulta
-        $stmt->execute();
-
-        // Obtendo o resultado da consulta
-        $result = $stmt->get_result();
-
-        // Verificando se a consulta retornou algum resultado
-        if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
-            // Verificando se a senha está correta
-            if (password_verify($senha, $row['senha'])) {
-                // Remova 'senha' do array antes de retornar
-                unset($row['senha']);
-                // Retorna o array do usuário
-                return $row;
-            }
-        }
-
-        // Se o usuário não existe ou a senha está incorreta, retorna null
-        return null;
+    public function obterCNPJ($cnpj) {
+        $url = $this->url_api . "cnpj/v1/" . $cnpj;
+        $response = $this->curl->get($url);
+        return json_decode($response, true);
     }
+
+    public function obterCEP($cep) {
+        $url = $this->url_api . "cep/v1/" . $cep;
+        $response = $this->curl->get($url);
+        return json_decode($response, true);
+    }
+
+    public function obterDDD($ddd) {
+        $url = $this->url_api . "ddd/v1/" . $ddd;
+        $response = $this->curl->get($url);
+        return json_decode($response, true);
+    }
+
+    // Adicione outras funções conforme necessário para obter informações adicionais
 }
-
-?>
